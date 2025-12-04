36ca0ba0e5e9e8f74a4a5ad2b25cb38cd43b3316
Stephen Griffin <stephengriffin2210@gmail.com>
2025-12-04 10:08:47 -0500

    CHORE: add data citation

--- nfl-prediction/src/App.tsx
+++ nfl-prediction/src/App.tsx
@@ -5,6 +5,7 @@ import { IoIosHome } from "react-icons/io";
 import { FaDatabase, FaUserCircle } from "react-icons/fa";
 import { IoAnalytics } from "react-icons/io5";
 import { introToData } from "./content/Intro";
+import { dataCuration, dataCitation } from "./content/DataCuration"
 
 const handleAnimationComplete = () => {
   console.log("All letters have animated!");
@@ -91,6 +92,11 @@ function App() {
         <div className="max-w-3xl px-6 text-center">
           <h2 className="text-3xl font-semibold mb-4">Data</h2>
           <p className="text-grey-700">{introToData}</p>
+          <br></br>
+          <p className="text-grey-700">{dataCuration}</p>
+          <br></br>
+          <p>{dataCitation}</p>
+          <a href="https://nflverse.com">nflverse.com</a>
         </div>
       </section>
 
--- nfl-prediction/src/content/DataCuration.ts
+++ nfl-prediction/src/content/DataCuration.ts
@@ -1,1 +1,3 @@
-export const dataCuration = 'For the model, we used nflreadr.';
+export const dataCuration = 'For this project we used nflreadr, which contains play by play data on NFL games since 1999. It is a package within the wider nflverse, created by five individual contributors - Sebastian Carl, Ben Baldwin, Lee Sharpe, Tan Ho, and John Edwards.';
+
+export const dataCitation = 'Carl S., & Baldwin B., & Sharpe L., & Ho T., & Edwards J. (2020). NFLVERSE';
